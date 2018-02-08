334224fa293a279e0f0f6d85da9b06f7053dc83a
al8876 <al8876@gmail.com>
2018-02-08 22:22:27 +0000

    slide toggle feature implemented to open category list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,27 @@
 //     }
 //   });;
 // });
+
+$(document).ready(function() {
+  
+  $('#toWatch').click(function(){
+    $('.watch .list').slideToggle('fast');
+  });
+
+  $('#toRead').click(function(){
+    $('.read .list').slideToggle('fast');
+  });
+
+  $('#toEat').click(function(){
+    $('.eat .list').slideToggle('fast');
+  });
+
+  $('#toBuy').click(function(){
+    $('.buy .list').slideToggle('fast');
+  });
+
+  $('#toOther').click(function(){
+    $('.other .list').slideToggle('fast');
+  });
+
+});
