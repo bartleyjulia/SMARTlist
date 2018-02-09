2ed985ffa229fc0ff6f171e4af62414ab218c397
al8876 <al8876@gmail.com>
2018-02-09 06:53:18 +0000

    bootstrap feature collapse and modal added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,37 +10,10 @@
 // });
 
 $(document).ready(function() {
-  
-  $('#toWatch').click(function(){
-    $('.watch .list-ai').slideToggle('fast');
-    console.log('test');
-  });
 
-  $('#toRead').click(function(){
-    $('.read .list-ai').slideToggle('fast');
-  });
-
-  $('#toEat').click(function(){
-    $('.eat .list-ai').slideToggle('fast');
-  });
-
-  $('#toBuy').click(function(){
-    $('.buy .list-ai').slideToggle('fast');
-  });
-
-  $('#toOther').click(function(){
-    $('.other .list-ai').slideToggle('fast');
-  });
-
-  $('.listItem').click(function() {
-    $('.modal, .modal-content').addClass('active');
-  });
-
-  $('.modal-close').click(function(e) {
+  $('.check').click(function(e) {
     e.stopPropagation();
-    $('.modal, .modal-content').removeClass('active');
   });
-
 
 });
 
