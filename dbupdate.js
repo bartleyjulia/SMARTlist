8278b5f7d601772d7e0bc99103d7538042e1a190
Julia <bartleyjulia@gmail.com>
2018-02-10 04:03:50 +0000

    Routes updated with skeletion Edit/Delete endpoints

---
+++ dbupdate.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME,
+    port     : process.env.DB_PORT,
+    ssl      : process.env.DB_SSL
+  }
+});
+
+function deleteItemFromDatabase(item_identifier) {
+  const promise = new Promise( function(resolve, reject) {
+    knex("user_lists").where("list_item", "item_identifier").del().asCallback(function(err, result ){
+       if (err) {
+        console.log(err);
+        return reject(err);
+      }
+      resolve();
+      console.log('Deleted!');
+    });
+
+  });
+  return promise;
+}
+
+exports.deleteItemFromDatabase = deleteItemFromDatabase;
--- routes/users.js
+++ routes/users.js
@@ -19,16 +19,37 @@ module.exports = (getListItemData, saveInputToDatabase) => {
   });
 
   router.post("/", (req, res) => {
-    // Pseudo code for receivng text input from '/'
+    // Code for receivng text input from input form
     const item = req.body['input_list_item'];
-    saveInputToDatabase(item);
-    // console.log(req);
     // send the text data to the database
+    saveInputToDatabase(item);
 
-    // re render '/'
     res.redirect('/');
 
   });
+
+  router.post("/delete/:id", (req, res) => {
+    // NOT DONE **** CHECK variable names/ PROCESS
+
+    {
+    deleteItemFromDatabase(item_identifier);
+    }
+
+     res.redirect('/');
+
+  });
+
+  router.post("/edit/:id", (req, res) => {
+    // NOT DONE **** CHECK variable names
+    new_category = req.body.category;
+    item_id = req.params.id;
+    {
+    deleteItemFromDatabase(item_identifier);
+    }
+     res.redirect('/');
+
+  });
+
 
   return router;
 };
